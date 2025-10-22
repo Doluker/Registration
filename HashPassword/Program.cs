01ac00825331aa6b222c87194d07a79d1106b69c
Doluker <kakafka1842@gmail.com>
2025-10-22 16:06:07 +0700

    Added Registration

---
+++ HashPassword/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using HashPassword.Models;
+using HashPasswords;
+
+namespace HashPassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Создание новой учетной записи для пользователя \n");
+            using (LeasingCompanyEntities db = new LeasingCompanyEntities().GetContext())
+            {
+                Console.Write("Введите фамилию пользователя:");
+                string surname = Console.ReadLine();
+                Console.Write("Введите имя пользователя:");
+                string name = Console.ReadLine();
+                User user = new User();
+                Employees employee = new Employees();
+                if (!string.IsNullOrWhiteSpace(name) && !string.IsNullOrWhiteSpace(surname))
+                {
+                    employee = db.Employees.FirstOrDefault(e =>
+                    e.Name == name &&
+                    e.Surname == surname
+                    );
+                }
+                if (employee == null)
+                {
+                    Console.WriteLine("Сотрудник с указанными ФИО не найден.");
+                    return;
+                }
+                if (db.User.FirstOrDefault(u => u.EmployeeId == employee.id) == null)
+                {
+                    while (true)
+                    {
+                        user.EmployeeId = employee.id;
+                        Console.Write("Введите логин пользователя:");
+                        user.Login = Console.ReadLine();
+                        if (db.User.FirstOrDefault(u => u.Login.Equals(user.Login)) == null)
+                        {
+                            while (true)
+                            {
+                                Console.Write("Введите пароль пользователя:");
+                                user.HashedPassword = Hash.PasswordHash(Console.ReadLine());
+                                if (user.HashedPassword == string.Empty)
+                                {
+                                    Console.WriteLine("Вы не ввели пароль");
+                                    continue;
+                                }
+                                break;
+                            }
+                            db.User.Add(user);
+                            try
+                            {
+                                db.SaveChanges();
+                                Console.WriteLine("Пользователь успешно добавлен");
+                                break;
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine($"Ошибка: {e.Message}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Логин существует");
+                            continue;
+                        }
+                    }
+                }
+                else 
+                {
+                    Console.WriteLine("Пользователь с таким ФИО существует");
+                }
+            }
+        }
+    }
+}
---
+++ HashPasswords/Hash.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace HashPasswords
+{
+    public class Hash
+    {
+        public static string PasswordHash(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                return string.Empty;
+            }
+            try
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(password);
+                using (SHA256 sha256 = SHA256.Create())
+                {
+                    byte[] hasBytes = sha256.ComputeHash(bytes);
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < hasBytes.Length; i++)
+                    {
+                        sb.Append(hasBytes[i].ToString("x2").ToUpper());
+                    }
+                    return sb.ToString();
+                }
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine($"Ошибка:{e.Message}");
+                return string.Empty;
+            }
+        }
+    }
+}
